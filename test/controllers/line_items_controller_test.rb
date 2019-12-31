c2a7d2c21405b9b68a41f217a72333350b72b31b
Kenneth Barrett <kenneth.barrett.jr@gmail.com>
2019-12-30 23:38:34 -0500

    Add quantity to line_items and combine line items to quantity

--- test/controllers/line_items_controller_test.rb
+++ test/controllers/line_items_controller_test.rb
@@ -21,10 +21,10 @@ class LineItemsControllerTest < ActionDispatch::IntegrationTest
       post line_items_url, params: { product_id: products(:ruby).id }
     end
     follow_redirect!
-    
+
     # assert_redirected_to line_item_url(LineItem.last)
     assert_select 'h2', 'Your Pragmatic Cart'
-    assert_select 'li', 'Programming Ruby 1.9'
+    assert_select 'li', "1 \u00D7 Programming Ruby 1.9"
   end
 
   test "should show line_item" do
